a1b5b5e12c5d10c51335f50b089c94c2161d3c3c
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-22 15:53:18 -0600

    code refactored

---
+++ input.js
@@ -0,0 +1,25 @@
+
+
+
+const handleUserInput = function(data) {
+  // your code here
+  // \u0003 maps to ctrl+c input
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {setupInput};
