d114693f356222fecca9ed84d635bf65e076a842
Tejasva-Bhatia <bhatiatejasva15@gmail.com>
2023-11-22 17:01:22 -0600

    added wasd movements

--- client.js
+++ client.js
@@ -40,4 +40,4 @@ const connect = function() {
   return conn;
 };
 
-module.exports = {connect};
+module.exports = { connect };
--- input.js
+++ input.js
@@ -1,4 +1,5 @@
-
+// Stores the active TCP connection object.
+let connection;
 
 
 const handleUserInput = function(data) {
@@ -7,11 +8,24 @@ const handleUserInput = function(data) {
   if (data === '\u0003') {
     process.exit();
   }
+  if (data === 'w') {
+    connection.write("Move: up");
+  }
+  if (data === 'a') {
+    connection.write("Move: left");
+  }
+  if (data === 's') {
+    connection.write("Move: down");
+  }
+  if (data === 'd') {
+    connection.write("Move: right");
+  }
 };
 
 // setup interface to handle user input from stdin
 
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -22,4 +36,4 @@ const setupInput = function() {
   return stdin;
 };
 
-module.exports = {setupInput};
+module.exports = { setupInput };
